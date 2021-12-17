f291b92f685a916b824c2ad2fec4590791d6c5bf
Hamiller78 <Kneesch@gmx.de>
2021-12-17 15:55:48 +0100

    Add interfaces for sensosr data sources and plant overviews

---
+++ PlantMonitorWebApp/Shared/Interfaces/IDataSource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantMonitorWebApp.Shared.Interfaces
+{
+    public interface IDataSource
+    {
+        double GetCurrentValue();
+    }
+}
---
+++ PlantMonitorWebApp/Shared/Interfaces/ISensorConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace PlantMonitorWebApp.Shared.Interfaces
+{
+    public interface ISensorConfiguration
+    {
+        public string Name { get; set; }
+        public Image Icon { get; set; }
+        public IDataSource SensorSource { get; set; }
+
+    }
+}
