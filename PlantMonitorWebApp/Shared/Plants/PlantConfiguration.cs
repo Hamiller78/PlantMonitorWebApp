73a2ac45d2cb5e8b86d87c093ec9e7e5e2ae6c33
Torben Kneesch <Kneesch@gmx.de>
2021-12-30 23:28:59 +0100

    Add datagrid for plant surveillance

---
+++ PlantMonitorWebApp/Shared/Plants/PlantConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PlantMonitorWebApp.Shared.Interfaces;
+
+namespace PlantMonitorWebApp.Shared.Plants
+{
+    public class PlantConfiguration
+    {
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        // public Image Icon { get; set; }
+        //public IDataSource SensorSource { get; set; } = null;
+    }
+}
---
+++ PlantMonitorWebApp/Shared/TestClasses/TestPlantProvider.cs
@@ -0,0 +1,20 @@
+﻿using PlantMonitorWebApp.Shared.Interfaces;
+using PlantMonitorWebApp.Shared.Plants;
+
+namespace PlantMonitorWebApp.Shared.TestClasses
+{
+    public static class TestPlantProvider
+    {
+        public static IEnumerable<PlantConfiguration> GetTestPlantConfigurations()
+        {
+            var plantList = new List<PlantConfiguration>
+            {
+                new PlantConfiguration() { Name = "Judith", Description = "Weihnachtsstern" },
+                new PlantConfiguration() { Name = "Mister Nancy", Description = "Palme" },
+                new PlantConfiguration() { Name = "Harry", Description = "Philodendron" }
+            };
+
+            return plantList;
+        }
+    }
+}
