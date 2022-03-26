e8fafa0e76601b1aeded0965e8d4be810201510c
Torben Kneesch <Kneesch@gmx.de>
2022-03-26 13:20:01 +0100

    Add Plant fetcher in Client

---
+++ PlantMonitorWebApp/Client/DataFetcher/PlantFetcher.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using PlantMonitorWebApp.Client.Interfaces;
+using PlantMonitorWebApp.Shared.Models;
+
+namespace PlantMonitorWebApp.Client.DataFetcher
+{
+    public class PlantFetcher : IPlantFetcher
+    {
+        HttpClient _client;
+
+        public PlantFetcher(HttpClient client) => _client = client;
+
+        public async Task<IEnumerable<Plant>> FetchPlantsAsync()
+        {
+            var plants = await _client.GetFromJsonAsync<IEnumerable<Plant>>("Plants") ?? new List<Plant>();
+            return plants;
+        }
+
+        public async Task<Plant?> GetPlantByIdAsync(int id)
+        {
+            var plant = await _client.GetFromJsonAsync<Plant>($"Plants/Item/{id.ToString().Trim()}");
+            return plant;
+        }
+    }
+}
---
+++ PlantMonitorWebApp/Server/Controllers/Plants.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using PlantMonitorWebApp.Repository.Interfaces;
+using PlantMonitorWebApp.Shared.Models;
+
+namespace PlantMonitorWebApp.Server.Controllers
+{
+    public class Plants : ControllerBase
+    {
+        readonly IPlantInterface _plantRepo;
+
+        public Plants(IPlantInterface plantRepo) => _plantRepo = plantRepo;
+
+        // GET: Plants/List
+        public IActionResult List()
+        {
+            return Ok(_plantRepo.GetAll());
+        }
+
+        // GET: Plants/Item/5
+        public IActionResult Item(int id)
+        {
+            Plant? plant = _plantRepo.GetById(id);
+            if (plant == null)
+            {
+                return NotFound();
+            }
+            return Ok(plant);
+        }
+/*
+        // GET: PlantsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PlantsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PlantsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PlantsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PlantsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PlantsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+*/
+    }
+}
